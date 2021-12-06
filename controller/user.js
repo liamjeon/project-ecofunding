2cbbe58553a3a2c5ee585df50112a6a6edd880d5
JooYoung2274 <wndud2274@gmail.com>
2021-12-06 23:46:37 +0900

    bcrypt update

--- controller/user.js
+++ controller/user.js
@@ -1,7 +1,14 @@
 import {} from "express-async-errors";
 import jwt from "jsonwebtoken";
 import * as userModel from "../model/user.js";
+import bcrypt from "bcrypt";
 
+// hash const
+const SECRET_KEY = "ecofunding";
+const EXPIRED = "1d";
+const SALT = 10;
+
+// loginId dup check
 export async function checkLoginId(req, res) {
   const loginId = req.body;
   const checkId = await userModel.findDup(loginId);
@@ -18,9 +25,9 @@ export async function checkLoginId(req, res) {
   }
 }
 
+// nickname dup check
 export async function checkNickname(req, res) {
   const nickname = req.body;
-  console.log(nickname);
   const checkNick = await userModel.findDup(nickname);
   if (checkNick) {
     res.status(400).send({
@@ -35,23 +42,27 @@ export async function checkNickname(req, res) {
   }
 }
 
+// signup
 export async function signup(req, res) {
   const { loginId, password, nickname } = req.body;
-  console.log(loginId);
-  const user = await userModel.createUser(loginId, password, nickname);
+  const hashPassword = await bcrypt.hash(password, SALT);
+  const user = await userModel.createUser(loginId, hashPassword, nickname);
   res.status(201).send({
     message: "회원가입 성공",
   });
 }
 
+//login
 export async function login(req, res) {
   const { loginId, password } = req.body;
-  console.log(loginId);
-  const userCheck = await userModel.findDup({ loginId, password });
-  if (!userCheck) {
-    res.status(400).send({ message: "닉네임 또는 패스워드를 확인해주세요" });
+  const userCheck = await userModel.findDup({ loginId });
+  const validPassword = await bcrypt.compare(password, userCheck.password); //bcrypt는 단방향 암호화라서 복화하가 불가능
+  if (!validPassword) {
+    res.status(400).send({ message: "아이디 또는 패스워드를 확인해주세요" });
     return;
   }
-  const token = jwt.sign({ userId: userCheck.userId }, "ecofunding");
+  const token = jwt.sign({ userId: userCheck.userId }, SECRET_KEY, {
+    expiresIn: EXPIRED,
+  });
   res.status(201).send({ token });
 }
--- model/user.js
+++ model/user.js
@@ -4,6 +4,15 @@ export async function findDup(input) {
   return Users.findOne(input);
 }
 
-export async function createUser(loginId, password, nickname) {
-  return new Users({ loginId, password, nickname, point: 100000 }).save();
+export async function createUser(loginId, hashPassword, nickname) {
+  return new Users({
+    loginId,
+    password: hashPassword,
+    nickname,
+    point: 100000,
+  }).save();
+}
+
+export async function findById(id) {
+  return Users.findById(id);
 }
