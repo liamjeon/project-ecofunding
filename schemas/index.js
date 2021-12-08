5e164d6bb1064ecef0f4d1c93e04abadad0cc996
JooYoung2274 <wndud2274@gmail.com>
2021-12-08 12:55:20 +0900

    DB update

--- schemas/index.js
+++ schemas/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 // 몽구스 연결 코드
 
-export function connect(){
+export function connect() {
   // 개발할때는 디버그모드로 설정 가능.
   if (process.env.NODE_ENV !== "production") {
     mongoose.set("debug", true);
@@ -10,14 +10,14 @@ export function connect(){
 
   //AWS 연결할때는 수정해야함.
   mongoose
-    .connect("mongodb://localhost:27017/ecofunding", {
+    // .connect("mongodb://localhost:27017/ecofunding", {
+    .connect("mongodb://test:test@13.125.141.36:27017/Eco_funding", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       ignoreUndefined: true,
     })
     .catch((err) => console.log(err));
-};
-
+}
 
 // 몽고디비 연결 에러
 mongoose.connection.on("error", (err) => {
