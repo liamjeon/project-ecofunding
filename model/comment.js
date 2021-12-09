94744b69296d8ece00cb37bad60f41850e3763c3
liamjeon <89363456+liamjeon@users.noreply.github.com>
2021-12-09 14:32:40 +0900

    comment update

--- model/comment.js
+++ model/comment.js
@@ -3,7 +3,37 @@ import SQ from "sequelize";
 const Sequelize = SQ.Sequelize;
 
 //Todo: Item model Import
+
+const INCLUDE_ITEM = {
+  attributes: [
+    "id",
+    "comment",
+    "rawDate",
+    "itemId",
+    [Sequelize.col("item.nickname"), "nickname"],
+  ],
+  include: {
+    // model: Item, //Todo: Item model Import
+    attributes: [],
+  },
+};
+const ORDER_DESC = {
+  order: [["createdAt", "DESC"]],
+};
+
 export async function getByitemId(itemId) {
+  return Comment.findAll({
+    ...INCLUDE_ITEM,
+    ...ORDER_DESC,
+    include: {
+      ...INCLUDE_ITEM.include, //기존 INCLUDE_ITEM.include 을 유지하면서
+      where: { itemId }, //itemId 조건 추가
+    },
+  }).then((data) => {
+    console.log(data);
+    return data;
+  });
+
   return Comment.findAll({ where: { itemId } });
 }
 
