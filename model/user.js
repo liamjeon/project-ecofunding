9564716c6725137509a09962416a0e38e0e3e247
JooYoung2274 <wndud2274@gmail.com>
2021-12-10 18:31:25 +0900

    update

--- model/user.js
+++ model/user.js
@@ -1,29 +1,27 @@
 import Users from "../schemas/user.js";
 import { buyUserCalculator } from "../utils/price.js";
-import SQ from "sequelize";
-const Sequelize = SQ.Sequelize;
 
 export async function findDup(input) {
-  return Users.findOne({ where: input });
+  return Users.findOne(input);
 }
 
 export async function createUser(loginId, hashPassword, nickname) {
-  return Users.create({
+  return new Users({
     loginId,
     password: hashPassword,
     nickname,
     point: 100000,
-  });
+  }).save();
 }
 
 export async function findById(id) {
-  return Users.findByPk(id);
+  return Users.findById(id);
 }
 
 export async function pointUpdateUser(id, point, price) {
   try {
     const newPoint = buyUserCalculator(price, point);
-    await Users.update({ point: newPoint }, { where: { id } });
+    await Users.updateOne({ _id: id }, { $set: { point: newPoint } }).exec();
     return;
   } catch (error) {
     console.log(error);
